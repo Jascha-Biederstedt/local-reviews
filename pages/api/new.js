64e0319c08158530d3089aa903f100efa7e9dd02
Jascha-Biederstedt <j.biederstedt@gmx.de>
2022-08-08 09:34:31 +0200

    Add the new API

---
+++ pages/api/new.js
@@ -0,0 +1,35 @@
+import { getSession } from 'next-auth/react';
+
+import prisma from 'lib/prisma';
+
+const handler = async (req, res) => {
+  if (req.method !== 'POST') {
+    return res.status(501).end();
+  }
+
+  const session = await getSession({ req });
+  if (!session) return res.status(401).json({ message: 'Not logged in' });
+
+  const user = await prisma.user.findUnique({
+    where: {
+      id: session.user.id,
+    },
+  });
+
+  if (!user) return res.status(401).json({ message: 'User not found' });
+
+  if (!user.isAdmin) return res.status(401).json({ message: 'Not authorized' });
+
+  await prisma.item.create({
+    data: {
+      name: req.body.name,
+      description: req.body.description,
+      type: req.body.type,
+      rating: 0,
+    },
+  });
+
+  res.end();
+};
+
+export default handler;
