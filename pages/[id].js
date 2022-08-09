3dfee63433f02d730bc9c1469802e06c426cd2a1
Jascha-Biederstedt <j.biederstedt@gmx.de>
2022-08-09 10:26:37 +0200

    Create pages/[id].js

---
+++ pages/[id].js
@@ -0,0 +1,24 @@
+import prisma from 'lib/prisma';
+import { getItem } from 'lib/data.js';
+
+export const getServerSideProps = async ({ params }) => {
+  const item = await getItem(prisma, parseInt(params.id));
+
+  return {
+    props: {
+      item,
+    },
+  };
+};
+
+const Item = ({ item }) => {
+  return (
+    <div className='text-center'>
+      <h1 className='mt-10 font-extrabold text-2xl'>{item.name}</h1>
+      <h2 className='mt-10 font-bold'>{item.description}</h2>
+      <h2 className='mt-10 font-bold'>{item.rating}</h2>
+    </div>
+  );
+};
+
+export default Item;
