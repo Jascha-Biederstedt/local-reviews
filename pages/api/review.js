f22c36220ab9e28a4aa2c23d7648dcf910568860
Jascha-Biederstedt <j.biederstedt@gmx.de>
2022-08-10 09:41:06 +0200

    Create post api route api/reviews.js

---
+++ pages/api/review.js
@@ -0,0 +1,58 @@
+import { getSession } from 'next-auth/react';
+
+import prisma from 'lib/prisma';
+
+const handler = async (req, res) => {
+  if (req.method !== 'POST') {
+    return res.status(501).end();
+  }
+
+  const session = await getSession({ req });
+  if (!session) return res.status(401).json({ message: 'Not logged in' });
+
+  const user = await prisma.user.findUnique({
+    where: {
+      id: session.user.id,
+    },
+  });
+
+  if (!user) return res.status(401).json({ message: 'User not found' });
+
+  await prisma.review.create({
+    data: {
+      description: req.body.description,
+      rating: parseInt(req.body.rating),
+      item: {
+        connect: { id: req.body.item },
+      },
+    },
+  });
+
+  const reviews = await prisma.review.findMany({
+    where: {
+      item: {
+        id: req.body.item,
+      },
+    },
+  });
+
+  const ratingsValues = reviews.reduce((acc, review) => {
+    console.log(acc, review.rating);
+    return acc + review.rating;
+  }, 0);
+
+  const rating = ratingsValues / reviews.length;
+
+  await prisma.item.update({
+    data: {
+      rating: Math.floor(rating * 10),
+    },
+    where: {
+      id: req.body.item,
+    },
+  });
+
+  res.end();
+};
+
+export default handler;
